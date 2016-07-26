73538d9eacb86a807bbc79a1b5709fac5fdcc711
besja <besja@yandex.ru>
2016-07-26 16:26:24 +0300

    validate workshop field

--- vfu/events/roundtable.py
+++ vfu/events/roundtable.py
@@ -3,6 +3,7 @@ from five import grok
 from zope import schema
 from plone.directives import dexterity, form
 from plone.dexterity.content import Container
+from zope.interface import invariant, Invalid
 
 from datetime import timedelta  
 
@@ -11,6 +12,16 @@ from vfu.events import MessageFactory as _
 from vfu.events.utils import daterange
 
 from plone.api.portal import get_localized_time
+import re
+
+def validate_workshops(value):
+    if value:
+        lines = value.split('\n')
+        p = re.compile('[0-9]+\|.+$')
+        for l in lines:
+            if not p.match(l): 
+                raise Invalid(_(u'Invalid value for workshops'))
+    return True
 
 class IVFURoundtableEvent(form.Schema):
     """
@@ -29,8 +40,8 @@ class IVFURoundtableEvent(form.Schema):
     vegfood_available  = schema.Bool(title=_(u'Is vegeterian food available?'), required=False, default=True)
     dinner_description = schema.Text(title=_(u"Description for dinner"), required=False)
 
-    workshops = schema.Text(title=_(u"Workshops"), description=_(u"One per line. Format: number|name. For example: 1|Workshop1"), required=False)
-    workshops_description = schema.Text(title=_(u"Description for workshops"), description=_(u"One per line"), required=False)
+    workshops = schema.Text(title=_(u"Workshops"), description=_(u"One per line. Format: number|name. For example: 1|Workshop1"), required=False, constraint=validate_workshops)
+    workshops_description = schema.Text(title=_(u"Description for workshops"), required=False)
 
 
 class VFURoundtableEvent(Container):
