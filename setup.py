1e1422f2a6f08670809788dbb29a5296599d0dd2
st009226 <i.bessarabova@spbu.ru>
2018-06-24 11:14:05 +0300

    minor chamges

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='vfu.events',
       # The next two lines may be deleted after you no longer need
       # addcontent support from paster and before you distribute
       # your package.
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
+      # setup_requires=["PasteScript"],
+      # paster_plugins = ["ZopeSkel"],
 
       )
--- vfu/events/roundtable_registration.py
+++ vfu/events/roundtable_registration.py
@@ -107,7 +107,7 @@ class IRoundtableRegistrationForm(form.Schema):
 
     # Contacts
     gender = schema.List(title=_(u'Gendre'), required=True, value_type=schema.Choice(source=gender), constraint=genderConstraint)
-    title_of_person = schema.List(title=_(u'Title'), required=True, value_type=schema.Choice(source=title), constraint=validate_title)
+    title_of_person = schema.List(title=_(u'Person Title'), required=True, value_type=schema.Choice(source=title), constraint=validate_title)
     lastname = schema.TextLine(title=_(u'Lastname'), required=True)
     firstname = schema.TextLine(title=_(u'Firstname'), required=True)
     organization = schema.TextLine(title=_(u'Organization'), required=False)
